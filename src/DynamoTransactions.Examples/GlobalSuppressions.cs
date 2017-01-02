721c69d2f8c8521a7c0a57ee0df7736980ec61bb
Timothy Adamson <timothy.adamson@gmail.com>
2017-01-03 05:00:27 +1100

    Add sonarlint suppressions

---
+++ src/DynamoTransactions.Examples/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "S2360:Optional parameters should not be used")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "S125:Sections of code should not be \"commented out\"")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "S108:Nested blocks of code should not be left empty")]
